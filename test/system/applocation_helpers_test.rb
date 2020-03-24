85cfc3fa47ff9d42bec2abb9586fda4cedf56aee
kamitoshi <kroo.aq.74@gmail.com>
2020-03-24 08:28:42 +0000

    Finish layout and routes

---
+++ test/system/applocation_helpers_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class ApplocationHelpersTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit applocation_helpers_url
+  #
+  #   assert_selector "h1", text: "ApplocationHelper"
+  # end
+end
