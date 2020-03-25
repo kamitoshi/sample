215b3c004e7bebdb60fec35a27404e5b97eb0b76
kamitoshi <kroo.aq.74@gmail.com>
2020-03-25 12:23:49 +0000

    test add

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,7 +7,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_url
     assert_response :success
-    assert_select "title", "Signup|#{@base_title}"
+    assert_select "title", "Sign up|#{@base_title}"
   end
 
 end
